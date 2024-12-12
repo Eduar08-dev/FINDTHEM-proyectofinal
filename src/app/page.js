b06c8258b7c575c304e9369c847ecab7ce5ca55f
Eduar08-dev <eduarm12@hotmail.com>
2024-12-11 23:16:27 -0500

    merge rama trabajo logica y master. Se corrige logica en formulario de creacion de usuario

--- src/app/page.js
+++ src/app/page.js
@@ -7,27 +7,26 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-white">
       <div className="bg-gradient-to-r from-blue-200 to-Azul-Suave py-8 my-1 px-4 rounded-lg shadow-md">
-      <div className="max-w-4xl mx-auto text-center">
-        <h1 className="text-4xl md:text-5xl font-extrabold text-gray-800 mb-4 flex items-center justify-center">
-          <Search className="mr-4 text-blue-600" size={48} />
-          Buscamos Esperanza
-          <Heart className="ml-4 text-red-500 animate-pulse" size={48} />
-        </h1>
-        <p className="text-xl text-gray-600 mb-6 max-w-2xl mx-auto">
-          Cada rostro cuenta una historia. Ayúdanos a reunir familias y regresar a quienes extraviamos.
-        </p>
-        <div className="flex justify-center items-center space-x-4">
-          <Users className="text-blue-500" size={32} />
-          <span className="text-lg font-semibold text-gray-700">
-            Juntos podemos marcar la diferencia
-          </span>
+        <div className="max-w-4xl mx-auto text-center">
+          <h1 className="text-4xl md:text-5xl font-extrabold text-gray-800 mb-4 flex items-center justify-center">
+            <Search className="mr-4 text-blue-600" size={48} />
+            Buscamos Esperanza
+            <Heart className="ml-4 text-red-500 animate-pulse" size={48} />
+          </h1>
+          <p className="text-xl text-gray-600 mb-6 max-w-2xl mx-auto">
+            Cada rostro cuenta una historia. Ayúdanos a reunir familias y regresar a quienes extraviamos.
+          </p>
+          <div className="flex justify-center items-center space-x-4">
+            <Users className="text-blue-500" size={32} />
+            <span className="text-lg font-semibold text-gray-700">
+              Juntos podemos marcar la diferencia
+            </span>
+          </div>
         </div>
+        <CarruselPublicaciones />
       </div>
-          <CarruselPublicaciones />
-    </div>
-          <Modulos />
-          <PrimeraSeccion />
-
+      <Modulos />
+      <PrimeraSeccion />
     </main>
   );
 }
